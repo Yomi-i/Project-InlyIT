9d7ca449faf2f3343243c80ec76ec2d7dfc9fb83
Yomi <79043238+Yomi-i@users.noreply.github.com>
2025-04-16 10:06:17 +0700

    Level boundaries for entities
    level boundaries 25x25 unimeters

---
+++ Assets/Scripts/LevelBoundaries.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBoundares : MonoBehaviour
+{
+    [SerializeField] private float _minX;
+    [SerializeField] private float _maxX;
+    [SerializeField] private float _minZ;
+    [SerializeField] private float _maxZ;
+
+    // References.
+    private Transform _characterTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterTransform = transform;
+    }
+
+    // Late update called after FixedUpdate so after Player did his move first.
+    void LateUpdate()
+    {
+        Vector3 currentPosition = _characterTransform.position;
+        currentPosition.x = Mathf.Clamp(currentPosition.x, _minX, _maxX);
+        currentPosition.z = Mathf.Clamp(currentPosition.z, _minZ, _maxZ);
+
+        if  (currentPosition != _characterTransform.position)
+        {
+            _characterTransform.position = currentPosition;
+        }
+    }
+    
+    // remove later
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(
+            new Vector3((_minX + _maxX) / 2, 0, (_minZ + _maxZ) / 2),
+            new Vector3(_maxX - _minX,  0.1f, _maxZ - _minZ)
+        );
+    }
+}
