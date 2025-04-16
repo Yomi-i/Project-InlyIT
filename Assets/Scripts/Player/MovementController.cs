7bf42433510949f6bdde1846e7ed60caafc4b63e
Yomi <79043238+Yomi-i@users.noreply.github.com>
2025-04-16 09:32:55 +0700

    Improved MovementController
    Now doesn't look choppy.
    Moving on FixedUpdate now.
    Minor code improvements.

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -9,55 +9,53 @@ public class MovementController : MonoBehaviour
     [SerializeField] private float rotationSpeed = 180f;
     [SerializeField] private float deadzone = 0.2f; // for OnScreen joystick controller.
     
+
     // References.
     private Rigidbody rb;
     private Transform characterTransform;
 
-    // Start is called before the first frame update
+
+    // Variables.
+    private Vector2 _playerInput;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         characterTransform = transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector2 playerInput = GetInput();
+        _playerInput = GetInput();
 
-        if (playerInput.magnitude > deadzone)
+        if (_playerInput.magnitude > deadzone)
         {
-            Move(playerInput);
-            RotateTowardsMovement(playerInput);
+            Move();
+            RotateTowardsMovement();
         }
         else StopMovement();
     }
 
     private Vector2 GetInput()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        return new Vector2(horizontal, vertical).normalized;
+        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
 
-    private void Move(Vector2 direction)
+    private void Move()
     {
-        Vector3 movementDirection =  new Vector3(direction.x, 0, direction.y); // Didn't used vector3 from beggining for RotateTowardsMovement, dont forget it.
+        Vector3 movementDirection =  new Vector3(_playerInput.x, 0, _playerInput.y); // Didn't used vector3 from beggining for RotateTowardsMovement, dont forget it.
         rb.MovePosition(transform.position + movementDirection * movementSpeed * Time.deltaTime);
     }
 
-    private void RotateTowardsMovement(Vector2 direction)
+    private void RotateTowardsMovement()
     {
-        if (direction.magnitude > 0)
-        {
-            float targetAngle = Mathf.Atan2(-direction.y, direction.x) * Mathf.Rad2Deg;
-            float rotation = Mathf.LerpAngle(
-                transform.rotation.eulerAngles.y,
-                targetAngle,
-                rotationSpeed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(0, rotation, 0);
-        }
+        float targetAngle = Mathf.Atan2(-_playerInput.y, _playerInput.x) * Mathf.Rad2Deg;
+        float rotation = Mathf.LerpAngle(
+            transform.rotation.eulerAngles.y,
+            targetAngle,
+            rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, rotation, 0);
     }
 
     private void StopMovement()
