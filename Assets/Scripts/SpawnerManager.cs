920176d8923f984546136d568b852f8807f987bd
Yomi <79043238+Yomi-i@users.noreply.github.com>
2025-04-17 15:14:30 +0700

    Update SpawnerManager.cs
    proper spawnermanager for prefabs

--- Assets/Scripts/SpawnerManager.cs
+++ Assets/Scripts/SpawnerManager.cs
@@ -1,17 +1,68 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpawnerManager : MonoBehaviour
 {
+    // Variables.
+    [Header("Spawner range")]
+    [SerializeField] private float _RangeX = 12.5f;
+    [SerializeField] private float _RangeZ = 12.5f;
+
+    [Header("Prefabs")]
     [SerializeField] private GameObject notePrefab;
     [SerializeField] private GameObject bombPrefab;
     [SerializeField] private GameObject healthPrefab;
     [SerializeField] private GameObject bonusPrafab;
+    private GameObject[] _availablePrefabs; // array of prefabs to spawn;
 
 
-    // Update is called once per frame
-    void Update()
+    // Flags.
+    private bool bIsSpawning = false;
+
+
+    void Start()
     {
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 10), 1, Random.Range(-10, 10));
-        Instantiate(notePrefab, randomSpawnPosition, Quaternion.identity);
+        _availablePrefabs = new GameObject[]
+        {
+            notePrefab,
+            bombPrefab,
+            healthPrefab,
+            bonusPrafab
+        };
+        StartCoroutine(
+                SpawnCooldown());
+    }
+    
+    IEnumerator SpawnCooldown()
+    {
+        while(true)
+        {
+            float waitTime = Random.Range(2f, 5f);
+            yield return new WaitForSeconds(waitTime);
+
+            SpawnRandomObject();
+        }
+    }
+
+    private void SpawnRandomObject()
+    {
+        if (bIsSpawning) return;
+
+        bIsSpawning = true;
+
+        GameObject randomPrefab = _availablePrefabs[Random.Range(0, _availablePrefabs.Length)];
+        if (randomPrefab)
+        {
+            Vector3 randomSpawnPos = new Vector3(Random.Range(-_RangeX, _RangeX), 1, Random.Range(-_RangeZ, _RangeZ));
+            Instantiate(randomPrefab, randomSpawnPos, Quaternion.identity);
+        }
+        else 
+        {
+            Debug.LogWarning("No prefabs to spawn");
+            bIsSpawning = false;
+            return;
+        }
+
+        bIsSpawning = false;
     }
 }
